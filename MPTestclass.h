6dbd277b0c86ed4ebaad87c742ed0c363cbc40cd
Andrei Muntean <andreimuntean@Andreis-MacBook-Pro.local>
2014-03-29 11:51:20 +0100

    added a test class

---
+++ MPTestclass.h
@@ -0,0 +1,13 @@
+//
+//  MPTestclass.h
+//  Git Test
+//
+//  Created by Andrei Muntean on 29/03/14.
+//  Copyright (c) 2014 Andrei Muntean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPTestclass : NSObject
+
+@end
---
+++ MPTestclass.m
@@ -0,0 +1,13 @@
+//
+//  MPTestclass.m
+//  Git Test
+//
+//  Created by Andrei Muntean on 29/03/14.
+//  Copyright (c) 2014 Andrei Muntean. All rights reserved.
+//
+
+#import "MPTestclass.h"
+
+@implementation MPTestclass
+
+@end
